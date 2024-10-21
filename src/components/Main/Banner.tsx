f1c8a93a271d5036514f7fe2bde043cb0a9395cf
tarpeeton <146853258+tarpeeton@users.noreply.github.com>
2024-10-21 12:11:23 +0500

    C:/Program Files/Git/baner / 1.1

--- src/components/Main/Banner.tsx
+++ src/components/Main/Banner.tsx
@@ -19,8 +19,7 @@ import { HiCursorClick } from "react-icons/hi"
 
 const Banner: FC = () => {
   return (
-    <div className='mt-[10px]  w-full overflow-hidden  2xl:h-auto'>
-      <div className='2xl:h-[725px] '>
+    <div className='mt-[10px]  w-full overflow-hidden '>
       <div className='flex flex-col w-full z-[9999] relative'>
         <h1 className=' w-full  text-[70px] mdl:text-[] text-center 2xl:text-[140px] text-titleDark font-bold font-manrope  uppercase'>Нозим Жумаев</h1>
         <p className=' mx-[13px] 2xl:w-[29%] 2xl:text-left 2xl:ml-[180px] 2xl:mx-0 text-center mt-[10px] text-[14px] font-semibold  text-[#686868]  2xl:text-[18px]'>Опытный бариатрический хирург с многолетней практикой. Специализируется на передовых методах хирургического лечения ожирения и смежных проблем</p>
@@ -28,32 +27,31 @@ const Banner: FC = () => {
 
 
 
-{/* Mobile elipse */}
-      <div className=' 2xl:hidden  h-[800px]   overflow-hidden absolute top-0'>
-        <Image src={MbLeft} alt='Ellipse 5' width={2300} height={3300} className='object-contain w-full  h-full  relative z-[99]' />
+      <div className=' 2xl:hidden  overflow-hidden absolute top-0'>
+        <Image src={MbLeft} alt='Ellipse 5' width={2300} height={3300} className='object-contain w-full   relative z-[999]' />
       </div>
-      <div className=' 2xl:hidden  h-[800px]   overflow-hidden absolute top-0 right-0'>
-        <Image src={MbRigth} alt='Ellipse 5' width={2300} height={3300} className='object-contain w-full  h-full  relative z-[99]' />
+      <div className=' 2xl:hidden  overflow-hidden absolute top-0 right-0'>
+        <Image src={MbRigth} alt='Ellipse 5' width={2300} height={3300} className='object-contain w-full   relative z-[999]' />
       </div>
 
-{/* desktop elipse */}
-      <div className='hidden   2xl:block rounded-[20px] overflow-hidden absolute bottom-0'>
+
+      <div className='hidden 2xl:block rounded-[20px] overflow-hidden absolute bottom-0'>
         <Image src={twoLeftImage} alt='Ellipse 5' width={2300} height={3300} className='object-contain w-full   relative z-[999]' />
       </div>
-      <div className='hidden   2xl:block rounded-[20px] overflow-hidden absolute bottom-0 right-0'>
+      <div className='hidden 2xl:block rounded-[20px] overflow-hidden absolute bottom-0 right-0'>
         <Image src={twoRigthImage} alt='Ellipse 5' width={2300} height={3300} className='object-contain w-full   relative z-[999]' />
       </div>
 
 
 
-      {/* image and button */}
-      <div className='relative mt-[10px]  z-[9999] 2xl:mt-[-230px] 2xl:ml-[-90px]'>
+
+      <div className='relative h-[407px] 2xl:h-auto mt-[10px] z-[9999] 2xl:mt-[-230px] 2xl:ml-[-100px]'>
         <Image src={NozimImage} alt='Ellipse 5' width={2300} height={3300} className='object-contain w-full  2xl:h-[800px] h-[450px] relative z-[999]' />
         {/*  ELLIPSE */}
-        <div className='absolute bottom-0   left-1/2 transform -translate-x-1/2 w-[300px] 2xl:w-[900px]'>
+        <div className='absolute bottom-0   left-1/2 transform -translate-x-1/2 w-[400px] 2xl:w-[900px]'>
           <Image src={ElTop} alt='Ellipse 5' width={2000} height={900} className='object-contain w-[550px]  h-full 2xl:w-[1000px]' />
         </div>
-        <div className='absolute  bottom-0  2xl:left-1/2 2xl:transform 2xl:-translate-x-1/2 2xl:h-[360px]'>
+        <div className='absolute w-[500px] left-1/2 transform -translate-x-1/2 bottom-0  2xl:left-1/2 2xl:transform 2xl:-translate-x-1/2 2xl:h-[360px]'>
           <Image src={ElBottomTop} alt='Ellipse 5' width={2000} height={1400} className='object-contain w-full h-full 2xl:w-[1500px]' />
         </div>
         {/* FORM BUTTON */}
@@ -93,8 +91,6 @@ const Banner: FC = () => {
 
         </div>
       </div>
-      </div>
-     
     </div>
   )
 }
--- src/components/Modal/ShowMore.tsx
+++ src/components/Modal/ShowMore.tsx
@@ -4,8 +4,6 @@ import { Modal } from 'antd'
 import { IoClose } from "react-icons/io5"
 import Image, { StaticImageData } from 'next/image'
 import { IShowMoreProps } from '@/interface/IReviews'
-import { PhotoProvider, PhotoView } from 'react-photo-view'
-import 'react-photo-view/dist/react-photo-view.css'
 import useLocale from '@/hooks/useLocale'
 import { IoSearchOutline } from "react-icons/io5"
 import FullScreenImage from './FullScreenImage'
@@ -45,7 +43,6 @@ const FullReviewsModal: FC<IReviewFull> = ({ visible, close, review }) => {
                         <div className='bg-white rounded-[20px] p-[20px] mt-[20px] mdl:w-[99%] 2xl:w-[99%]  2xl:h-[450px]'>
                             <div className='flex flex-col'>
                                 <div className='border-b pb-[30px] border-[#E3E3E3] flex flex-row gap-[10px]'>
-                                    <PhotoProvider>
                                         <div className='w-[100px] relative h-[100px] rounded-[20px] overflow-hidden group'>
                                             <div  className='absolute  w-full h-full bg-black bg-opacity-40 group-hover:hidden' />
                                             {review?.before.src && (
@@ -76,15 +73,12 @@ const FullReviewsModal: FC<IReviewFull> = ({ visible, close, review }) => {
                                                     </div>
                                                 </button>)}
                                             {review?.after.src && (
-                                                <PhotoView src={review?.after.src}>
                                                     <Image src={review?.after.src} alt='After' width={100} height={100} className='w-full h-full object-cover' />
-                                                </PhotoView>
                                             )}
                                             <p className='absolute bottom-[10px] text-[16px] 2xl:text-[18px] left-[10px] text-white font-extrabold font-manrope group-hover:hidden'>
                                                 После
                                             </p>
                                         </div>
-                                    </PhotoProvider>
                                 </div>
                                 {open && <FullScreenImage src={url} open={open} close={handleClose} />}
                                 <div className='flex flex-row gap-[8px] mt-[30px]'>
