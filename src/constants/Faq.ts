1f68487158b3974f723e91fbdc98f9607ead342c
tarpeeton <146853258+tarpeeton@users.noreply.github.com>
2024-10-28 12:50:49 +0500

    experince #uz

--- src/constants/Faq.ts
+++ src/constants/Faq.ts
@@ -2,51 +2,51 @@ export const faqData = [
   {
     question: {
       ru: "Как долго длится восстановление после бариатрической операции?",
-      uz: ""
+      uz: "Bariatrik jarrohlikdan keyin tiklanish qancha vaqt davom etadi?"
     },
     answer: {
       ru: "После операции пациент обычно проводит 2-3 дня в стационаре. Полное восстановление занимает около 2-4 недель, в зависимости от типа процедуры и индивидуальных особенностей организма.",
-      uz: ""
+      uz: "Jarrohlikdan keyin bemor odatda 2-3 kun kasalxonada yotadi. To‘liq tiklanish muolaja turiga va organizmning individual xususiyatlariga qarab  taxminan 2-4 hafta davom etadi."
     }
   },
   {
     question: {
       ru: "Сколько веса я смогу сбросить после операции?",
-      uz: ""
+      uz: "Jarrohlikdan keyin qancha vazn yo‘qotishim mumkin?"
     },
     answer: {
       ru: "Потеря веса зависит от выбранной процедуры и вашего исходного веса, но в среднем пациенты теряют 50-70% избыточной массы тела в течение первого года.",
-      uz: ""
+      uz: "Og‘irlikni yo‘qotish tanlangan muolajaga va dastlabki vazningizga bog‘liq, ammo o‘rtacha bemorlar birinchi yil davomida ortiqcha tana vaznining 50-70% yo‘qotadi."
     }
   },
   {
     question: {
       ru: "Какие осложнения могут возникнуть после операции?",
-      uz: ""
+      uz: "Jarrohlikdan keyin qanday asoratlar paydo bo‘lishi mumkin?"
     },
     answer: {
       ru: "Как и при любой хирургической процедуре, возможны риски, такие как инфекции, кровотечения или нарушение всасывания питательных веществ. Тем не менее, современные методики и квалификация хирурга сводят эти риски к минимуму.",
-      uz: ""
+      uz: "Har qanday jarrohlik amaliyotida bo‘lgani kabi infeksiyalar, qon ketishi yoki ozuqa moddalarining singishi buzilishi singari xavflar bo‘lishi mumkin. Shunga qaramay, jarrohning zamonaviy metodikasi va malakasi bu xavflarni minimallashtiradi."
     }
   },
   {
     question: {
       ru: "Можно ли забеременеть после бариатрической операции?",
-      uz: ""
+      uz: "Bariatrik operatsiyadan keyin homilador bo‘lish mumkinmi?"
     },
     answer: {
       ru: "Да, беременность возможна, но рекомендуется планировать ее не ранее чем через 12-18 месяцев после операции, когда вес стабилизируется, и организм полностью восстановится.",
-      uz: ""
+      uz: "Ha, homilador bo‘lish mumkin, ammo buni operatsiyadan 12-18 oy o‘tgach, vazn barqarorlashganda va organizm to‘liq tiklanganda rejalashtirish tavsiya etiladi."
     }
   },
   {
     question: {
       ru: "Какие диетические ограничения нужно соблюдать после операции?",
-      uz: ""
+      uz: "Jarrohlikdan keyin qanday ovqatlanish cheklovlariga rioya qilish kerak?"
     },
     answer: {
       ru: "После операции важно придерживаться специальной диеты, которая включает в себя маленькие порции мягкой и жидкой пищи в течение первых недель. Постепенно рацион расширяется, но важно избегать высококалорийных продуктов и соблюдать рекомендации диетолога.",
-      uz: ""
+      uz: "Jarrohlikdan so‘ng birinchi haftalarda yumshoq va suyuq ovqatlarni kichik porsiyalarda yeyish orqali  maxsus parhezga rioya qilish muhim. Asta-sekin ratsion kengayadi, ammo yuqori kaloriyali ovqatlardan yiroqlashish va diyetologning tavsiyalariga amal qilish muhim."
     }
   }
 ];
