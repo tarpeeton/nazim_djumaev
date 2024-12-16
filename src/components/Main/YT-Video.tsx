cd35c79b6b7bb85835f9dce5871b37930ddc6c73
tarpeeton <146853258+tarpeeton@users.noreply.github.com>
2024-12-16 11:25:25 +0500

    #About YTVideo triger time

--- src/components/Main/YT-Video.tsx
+++ src/components/Main/YT-Video.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 
 export default function YTVideo() {
   return (
-    <div className='mt-[100px] 2xl:mt-[170px] mx-[16px] mdl:mx-[180px]  h-[250px] mdl:h-[550px]'>
+    <div className='mt-[100px] 2xl:mt-[170px] mx-[16px] mdl:mx-[180px]  h-[300px] mdl:h-[550px]'>
       <iframe width="100%" height="100%" src="https://www.youtube.com/embed/51KHSufjPxg" title="Qanday qilib 100 KGgacha sog&#39;lom ozish mumkin!" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin"  allowFullScreen></iframe>
     </div>
   )
